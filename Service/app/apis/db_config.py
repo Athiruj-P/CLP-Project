b2c5133937c80246089d33c1ba4887d4203080bf
Athiruj Poositaporn <athiruj.poositaporn@gmail.com>
2021-01-06 12:32:54 +0700

    Add db config

--- Service/app/apis/db_config.py
+++ Service/app/apis/db_config.py
@@ -20,6 +20,45 @@ item = {
     "fld_user_LOGIN" : 1,
     "fld_user_LOGOUT" : 0,
 
+    # clp_user [planner] fields
+    "fld_pln_name" : "pln_name",
+    "fld_pln_width" : "pln_width",
+    "fld_pln_height" : "pln_height",
+    "fld_pln_depth" : "pln_depth",
+    "fld_pln_unit_id" : "pln_unit_id",
+    "fld_pln_created_date" : "pln_created_date",
+    "fld_pln_latest_updated" : "pln_latest_updated",
+    "fld_pln_status" : "pln_status",
+    "fld_pln_boxes" : "boxes",
+
+    # clp_box fields
+    "fld_box_name" : "box_name",
+    "fld_box_width" : "box_width",
+    "fld_box_height" : "box_height",
+    "fld_box_depth" : "box_depth",
+    "fld_box_unit_id" : "box_unit_id",
+    "fld_box_quantity" : "box_quantity",
+    "fld_box_color_id" : "box_color_id",
+    "fld_box_created_date" : "box_created_date",
+    "fld_box_latest_updated" : "box_latest_updated",
+
+    # clp_color fields
+    "fld_color_hex" : "color_hex",
+
+    # clp_box_std fields
+    "fld_box_std_name" : "box_std_name",
+    "fld_box_std_width" : "box_std_width",
+    "fld_box_std_height" : "box_std_height",
+    "fld_box_std_depth" : "box_std_depth",
+    "fld_box_std_unit_id" : "box_std_unit_id",
+    
+    # clp_container_std fields
+    "fld_con_std_name" : "con_std_name",
+    "fld_con_std_width" : "con_std_width",
+    "fld_con_std_height" : "con_std_height",
+    "fld_con_std_depth" : "con_std_depth",
+    "fld_con_std_unit_id" : "con_std_unit_id",
+
     # clp_unit fields
     "fld_role_id" : "role_id",
     "fld_role_name" : "role_name",
