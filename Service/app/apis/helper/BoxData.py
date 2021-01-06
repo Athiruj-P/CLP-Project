90a9a47857b1cdb7e5e02a9e03e496d167ae8ea2
Athiruj Poositaporn <athiruj.poositaporn@gmail.com>
2021-01-06 10:07:06 +0700

    Add helper class

---
+++ Service/app/apis/helper/BoxData.py
@@ -0,0 +1,11 @@
+# BoxData
+# Description : สำหรับเก็บข้อมูลนำเข้าของ 1 กล่องบรรจุสินค้า
+# Author : Athiruj Poositaporn
+
+from .PlannerData import PlannerData
+
+class BoxData(PlannerData):
+    def __init__(self, box_id=0, boxes=None):
+        super().__init__()
+        self.box_id = box_id 
+        self.boxes = boxes
---
+++ Service/app/apis/helper/PlannerData.py
@@ -0,0 +1,8 @@
+# PlannerData
+# Description : สำหรับเก็บข้อมูลนำเข้าของ 1 แผนการจัดเรียง
+# Author : Athiruj Poositaporn
+
+class PlannerData():
+    def __init__(self, user_id=0, planner_id=0):
+        self.user_id = user_id 
+        self.planner_id = planner_id 
