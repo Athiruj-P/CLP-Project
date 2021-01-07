a95f66a0b8ae98e57d66b3de21bb440ffee86033
Athiruj Poositaporn <athiruj.poositaporn@gmail.com>
2021-01-08 00:49:34 +0700

    Edit planner data

--- Service/app/apis/helper/PlannerData.py
+++ Service/app/apis/helper/PlannerData.py
@@ -3,6 +3,7 @@
 # Author : Athiruj Poositaporn
 
 class PlannerData():
-    def __init__(self, user_id=0, planner_id=0):
+    def __init__(self, user_id=0, planner_id=0, planner=None):
         self.user_id = user_id 
         self.planner_id = planner_id 
+        self.planner = planner 
