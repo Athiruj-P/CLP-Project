08e74318647f8d4e6864b994632005f0fef14c97
Athiruj Poositaporn <athiruj.poositaporn@gmail.com>
2021-01-10 16:38:22 +0700

    Edit helper "BoxData"

--- Service/app/apis/helper/BoxData.py
+++ Service/app/apis/helper/BoxData.py
@@ -5,7 +5,8 @@
 from .PlannerData import PlannerData
 
 class BoxData(PlannerData):
-    def __init__(self, box_id=0, boxes=None):
+    def __init__(self, box_id=0, boxes=None, excel_file=None):
         super().__init__()
         self.box_id = box_id 
         self.boxes = boxes
+        self.excel_file = excel_file
