ea117adc175c6ec090d320b1791edcd9d9a519c7
Athiruj Poositaporn <athiruj.poositaporn@gmail.com>
2021-01-11 14:39:38 +0700

    Edit global variable

--- Service/app/apis/packer/global_var.py
+++ Service/app/apis/packer/global_var.py
@@ -4,9 +4,11 @@ def init():
     global START_POSITION
     global USED_VOLUME
     global BOXES_STACK
+    global BOXES_STACK_DETAIL
     
     UNFITTED_ITEMS = [] # List of unfitted node
     BASE_BOXES = [] # Start node of each stack
     START_POSITION = [0, 0, 0]
     USED_VOLUME = 0
     BOXES_STACK = []
+    BOXES_STACK_DETAIL = []
