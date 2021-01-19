b0dc121060b2d73157fd44051f9bf44536567380
Athiruj Poositaporn <athiruj.poositaporn@gmail.com>
2021-01-20 01:54:10 +0700

    Add mixin "alert"

---
+++ clp_ui/mixins/alert.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  methods: {
+    show_alert(data) {
+      if (data.status === "error" || data.status === "system_error") {
+        this.$store.commit("set_alert_data", {
+          text_alert: data.mes,
+          color_alert: "red",
+          icon_alert: "fas fa-exclamation-circle"
+        });
+      } else {
+        this.$store.commit("set_alert_data", {
+          text_alert: data.mes,
+          color_alert: "success",
+          icon_alert: "fas fa-check-circle"
+        });
+      }
+      this.$store.commit("set_show_alert", true);
+    }
+  }
+};
+</script>
