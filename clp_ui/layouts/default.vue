8b4af29d8f0702bbd6f39ec1c1452ee527771f12
Athiruj Poositaporn <athiruj.poositaporn@gmail.com>
2021-01-20 01:54:56 +0700

    Add alert component to layouts

--- clp_ui/layouts/default.vue
+++ clp_ui/layouts/default.vue
@@ -1,8 +1,34 @@
 <template>
-  <div class="h-full overflow-y-hidden">
+  <v-app class="h-full overflow-y-hidden">
     <nuxt />
-  </div>
+    <Alert />
+  </v-app>
 </template>
+<script>
+import Alert from "@/components/Alert";
+
+export default {
+  async beforeMount() {
+    try {
+      var local_store_user_id = localStorage.getItem("user_id");
+      var local_store_username = localStorage.getItem("username");
+      if (local_store_user_id && local_store_username) {
+        if (!this.$store.state.user_id) {
+          this.$store.commit("set_login", {
+            statue: true,
+            username: local_store_username,
+            user_id: local_store_user_id
+          });
+        }
+      } else {
+        this.logout();
+      }
+    } catch {
+      this.logout();
+    }
+  }
+};
+</script>
 <style scoped>
 * {
   font-family: "Ubuntu";
