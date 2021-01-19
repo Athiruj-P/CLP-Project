1b232418108d94c3a8c1950246943161bc8c57df
Athiruj Poositaporn <athiruj.poositaporn@gmail.com>
2021-01-20 01:55:54 +0700

    Edit component "PlannerCard"

--- clp_ui/components/planner/PlannerCard.vue
+++ clp_ui/components/planner/PlannerCard.vue
@@ -1,19 +1,33 @@
 <template>
-  <v-card width="250" height="160" outlined elevation="3">
+  <v-card
+    width="250"
+    height="160"
+    outlined
+    elevation="3"
+    class="cursor-pointer"
+  >
     <v-list-item three-line>
       <v-list-item-content>
         <v-list-item-title class="headline mb-1 flex justify-space-between">
           <v-icon>fas fa-truck-loading</v-icon>
-          <EditDialog show="card" :obj_planner="planner"/>
+          <EditDialog show="card" :obj_planner="planner" />
           <!-- {{card_data}} -->
         </v-list-item-title>
-        <span class="text-sm text-gray-600 font-semibold">{{ planner.pln_name }}</span>
+        <span class="text-sm text-gray-600 font-semibold">{{
+          planner.pln_name
+        }}</span>
         <br />
-        <span class="text-sm text-gray-600">Width : {{ planner.pln_width }} {{ planner.pln_unit }}</span>
+        <span class="text-sm text-gray-600"
+          >Width : {{ planner.pln_width }} {{ planner.pln_unit }}</span
+        >
         <br />
-        <span class="text-sm text-gray-600">Heigth : {{ planner.pln_height }} {{ planner.pln_unit }}</span>
+        <span class="text-sm text-gray-600"
+          >Heigth : {{ planner.pln_height }} {{ planner.pln_unit }}</span
+        >
         <br />
-        <span class="text-sm text-gray-600">Depth : {{ planner.pln_depth }} {{ planner.pln_unit }}</span>
+        <span class="text-sm text-gray-600"
+          >Depth : {{ planner.pln_depth }} {{ planner.pln_unit }}</span
+        >
         <br />
       </v-list-item-content>
     </v-list-item>
@@ -33,7 +47,6 @@ export default {
   //     card_data: this.planner
   //   }),
   methods: {},
-  mounted: function() {
-  }
+  mounted: function() {}
 };
 </script>
