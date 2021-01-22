a838b5b770e3a169ac4dab263f27711e84062dca
Athiruj Poositaporn <athiruj.poositaporn@gmail.com>
2021-01-23 00:37:40 +0700

    Edit page "planner_manager"

--- clp_ui/pages/planner_manager/index.vue
+++ clp_ui/pages/planner_manager/index.vue
@@ -1,114 +1,147 @@
 <template>
-  <div class="h-full">
-    <v-col md="3" class="h-full px-0 py-0">
-      <v-navigation-drawer class="primary" permanent width="100%">
-        <!-- Header -->
-        <div class="px-2 py-2 flex flex-wrap items-center justify-around">
-          <div class="flex items-center">
-            <v-img
-              max-width="65px"
-              aspect-ratio="1"
-              :src="require('@/assets/img/clp_logo.png')"
-            ></v-img>
-            <span class="text-3xl font-black text-white">CLP</span>
-          </div>
+  <v-row class="h-full w-screen flex">
+    <v-col md="3" class="mx-0 my-0 pr-0">
+      <v-navigation-drawer class="primary sticky" permanent width="100%">
+        <div style="height:10%">
+          <!-- Header -->
+          <div class="px-2 py-2 flex flex-wrap items-center justify-around">
+            <div class="flex items-center">
+              <v-img
+                max-width="65px"
+                aspect-ratio="1"
+                :src="require('@/assets/img/clp_logo.png')"
+              ></v-img>
+              <span class="text-3xl font-black text-white">CLP</span>
+            </div>
 
-          <!-- Logout btn -->
-          <div>
-            <v-menu bottom offset-y>
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  class="hidden-xs-only"
-                  outlined
-                  color="white"
-                  v-bind="attrs"
-                  v-on="on"
-                >
-                  {{ $store.state.username }}
-                  <v-icon right>fas fa-user-circle</v-icon>
-                </v-btn>
-              </template>
+            <!-- Logout btn -->
+            <div>
+              <v-menu bottom offset-y>
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    class="hidden-xs-only"
+                    outlined
+                    color="white"
+                    v-bind="attrs"
+                    v-on="on"
+                  >
+                    {{ $store.state.username }}
+                    <v-icon right>fas fa-user-circle</v-icon>
+                  </v-btn>
+                </template>
 
-              <v-list>
-                <v-list-item>
-                  <v-list-item-title class="cursor-pointer" @click="logout">
-                    <span>
-                      <v-icon>fas fa-sign-out-alt</v-icon>
-                    </span>
-                    Logout
-                  </v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-menu>
+                <v-list>
+                  <v-list-item>
+                    <v-list-item-title class="cursor-pointer" @click="logout">
+                      <span>
+                        <v-icon>fas fa-sign-out-alt</v-icon>
+                      </span>
+                      Logout
+                    </v-list-item-title>
+                  </v-list-item>
+                </v-list>
+              </v-menu>
+            </div>
+            <!-- Logout btn -->
           </div>
-          <!-- Logout btn -->
+          <!-- Header -->
         </div>
-        <!-- Header -->
-
-        <!-- Back BTN -->
-        <div class="m-2 flex items-center">
-          <NuxtLink to="/planner"
-            ><v-icon color="white">fas fa-arrow-left</v-icon>
-            <span class="text-white mx-2 text-xl font-bold">Back</span>
-          </NuxtLink>
-          <!-- <div class="cursor-pointer">
+        <div style="height:73%;">
+          <!-- Back BTN -->
+          <div class="m-2 flex items-center">
+            <NuxtLink to="/planner"
+              ><v-icon color="white">fas fa-arrow-left</v-icon>
+              <span class="text-white mx-2 text-xl font-bold">Back</span>
+            </NuxtLink>
+            <!-- <div class="cursor-pointer">
             
           </div> -->
+          </div>
+          <!-- Back BTN -->
+
+          <v-divider color="white"></v-divider>
+
+          <!-- Planner btn -->
+          <div class="m-2 flex items-center justify-between">
+            <div>
+              <v-icon color="white">fas fa-truck-loading</v-icon>
+              <span
+                v-if="planner_name"
+                class="text-white mx-2 text-xl font-bold"
+              >
+                {{ planner_name }}
+              </span>
+            </div>
+            <div
+              v-if="$store.state.planner_manage.selected_planner"
+              class="text-white"
+            >
+              <EditDialog
+                show="manage"
+                :obj_planner="$store.state.planner_manage.selected_planner"
+              />
+            </div>
+          </div>
+          <!-- Planner btn -->
+          <v-divider color="white"></v-divider>
+
+          <!-- Box BTN -->
+          <div class="m-2 flex items-center justify-between">
+            <div>
+              <v-icon color="white">fas fa-box-open</v-icon>
+              <span class="text-white mx-2 text-xl font-bold">Box</span>
+            </div>
+            <div v-if="selected_planner" class="text-white">
+              <AddBoxDialog />
+            </div>
+          </div>
+          <!-- Box BTN -->
+          <v-divider color="white"></v-divider>
+
+          <!-- Box list -->
+          <BoxList />
+          <!-- Box list -->
         </div>
-        <!-- Back BTN -->
-
-        <v-divider color="white"></v-divider>
-
-        <!-- Planner btn -->
-        <div class="m-2 flex items-center justify-between">
-          <div>
-            <v-icon color="white">fas fa-truck-loading</v-icon>
-            <span v-if="planner_name" class="text-white mx-2 text-xl font-bold">
-              {{ planner_name }}
-            </span>
-          </div>
-          <div v-if="selected_planner" class="text-white">
-            <EditDialog show="manage" :obj_planner="selected_planner" />
-          </div>
-        </div>
-        <!-- Planner btn -->
-
-        <v-divider color="white"></v-divider>
-
-        <!-- Box BTN -->
-        <div class="m-2 flex items-center justify-between">
-          <div>
-            <v-icon color="white">fas fa-box-open</v-icon>
-            <span class="text-white mx-2 text-xl font-bold">Box</span>
-          </div>
-          <div v-if="selected_planner" class="text-white">
-            <AddBoxDialog />
-          </div>
-        </div>
-        <!-- Box BTN -->
-        <v-divider color="white"></v-divider>
-
-        <!-- Box list -->
-        <BoxList />
-        <!-- Box list -->
 
         <!-- Browse file and Render BTN -->
-        <template v-slot:append>
-          <div class="p-2">
-            <v-btn block>
-              Browse file
-            </v-btn>
+        <div style="height:17%" class="bg-gray-200">
+          <div
+            style="height:17%"
+            class="absolute bottom-0 left-0 flex flex-col justify-center w-full"
+          >
+            <div class="pb-2 pt-8 px-2">
+              <v-btn block elevation="2" color="indigo">
+                <span class="text-white">Browse file</span>
+              </v-btn>
+            </div>
+            <div class="pb-1 px-2">
+              <v-btn
+                v-if="$store.state.box.boxes.length > 0"
+                block
+                color="warning"
+              >
+                Render
+              </v-btn>
+              <div v-else class="cursor-not-allowed">
+                <v-btn block color="warning" :disabled="true">
+                  Render
+                </v-btn>
+              </div>
+            </div>
           </div>
-          <div class="p-2">
-            <v-btn block color="warning">
-              Render
-            </v-btn>
-          </div>
-        </template>
+        </div>
         <!-- Browse file and Render BTN -->
       </v-navigation-drawer>
     </v-col>
-  </div>
+    <v-col md="9" class="h-full pl-3 pr-1 flex flex-col">
+      <div class="h-3/4 pa-2">
+        <Graph3D />
+      </div>
+      <v-card style="height:30%" color="#E5E7EB" tile class="mb-3">
+        dddd
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -117,7 +150,9 @@ import login from "@/mixins/login";
 import EditDialog from "@/components/planner/EditDialog";
 import AddBoxDialog from "@/components/planner_manager/AddBoxDialog";
 import BoxList from "@/components/planner_manager/BoxList";
+import Graph3D from "@/components/planner_manager/Graph3D";
 export default {
+  layout: "planner_manager",
   mixins: [login, box],
   async beforeMount() {
     var local_store_selected_planner = localStorage.getItem("selected_planner");
