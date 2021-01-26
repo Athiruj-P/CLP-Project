e9dcc635e830e1564ff7f25bca3adacabc7ec8cb
Athiruj Poositaporn <athiruj.poositaporn@gmail.com>
2021-01-26 10:04:13 +0700

    Add mixins "stack"

---
+++ clp_ui/mixins/stack.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  methods: {
+    async render_container() {
+      let data = new FormData();
+      data.append("user_id", this.$store.state.user_id);
+      data.append(
+        "pln_id",
+        this.$store.state.planner_manage.selected_planner._id
+      );
+      let render_result = await this.$axios.$post("render_container", data);
+      this.$store.commit("planner_manage/set_render_data", render_result);
+      localStorage.removeItem("render_result");
+      localStorage.setItem("render_result", JSON.stringify(render_result));
+    }
+  }
+};
+</script>
