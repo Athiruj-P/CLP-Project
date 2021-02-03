fb74f398ce4740dba63485c3537e532db888fb2c
Athiruj Poositaporn <athiruj.poositaporn@gmail.com>
2021-02-04 01:27:31 +0700

    Add error msg

--- Service/app/apis/err_msg.py
+++ Service/app/apis/err_msg.py
@@ -9,10 +9,14 @@ msg = {
     'wrong_planner_id' : "The planner id does not exist.",
     'wrong_unit_id' : "Wrong unit id, this ID dose not match any unit id on clp_unit.",
     'wrong_color_id' : "Wrong color id, this ID dose not match any unit id on clp_unit.",
+    'wrong_lenght_cm' : "Length must contain minimum 1 cm and maximum 1,500 cm.",
+    'wrong_lenght_in' : "Length must contain minimum 0.39 inch and maximum 590 inch.",
+    'wrong_qty_size' : "Quantity must contain minimum 1 box and maximum 100 boxes.",
+    'wrong_decimal' : "Length must contain maximum 4 decimal digits.",
 
     # Error message for excel controller 
     'wrong_column_name' : 'Column name must be name, width, height, depth, qty and unit ',
-    'wrong_unit_abb' : 'Box unit must contain lenght abbreviation either cm or in',
+    'wrong_unit_abb' : 'Box unit must contain lenght abbreviation either cm or inch',
     'duplicate_name' : 'Each box name must be unique',
 
 
