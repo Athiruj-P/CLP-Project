3bd9ecb751ca8b11825039e01e0e11cf41973010
Athiruj Poositaporn <athiruj.poositaporn@gmail.com>
2021-03-17 22:33:42 +0700

    Delete comments

--- Service/app/apis/err_msg.py
+++ Service/app/apis/err_msg.py
@@ -19,40 +19,4 @@ msg = {
     'wrong_unit_abb' : 'Box unit must contain lenght abbreviation either cm or inch',
     'duplicate_name' : 'Each box name must be unique',
 
-
-    # Error message for image processing API
-    # 'other_err' : "Something went wrong, Please try again later.",
-    # 'file_empty' : "Image file is empty, Please upload an new image.",
-    # 'file_extension' : "Wrong file extension, Please upload an new image.",
-    # 'file_size' : "Image file size is more than 10 MB.",
-    # 'unit_id' : "Wrong unit. Please re-selecte unit.",
-    # 'ml_model_not_found' : "Object not detected, Please try again later.",
-    # 'ref_model_not_found' : "Reference object not detected, Please try again later.",
-    # 'no_active_ml_model' : "None of ML models have been activated.",
-    # 'no_active_ref_model' : "None of reference models have been activated.",
-
-    # Error message for ML / Ref
-    # 'wrong_extension' : "Wrong file extension. The model file must be *.weights",
-    # 'duplicate_name' : "This name has already taken, Please enter a unique model name.",
-
-    # Error message for ML model management
-    # 'ml_file_empty' : "ML model file is empty.",
-    # 'wrong_ml_name' : "Model name must contain minimum 3 characters and maximum 30 characters. English, Thai, number, a space ( ) and underscore (_) are allow.",
-    # 'wrong_mlmo_id' : "Wrong model id. This ID dose not match any unit id on dpml_ml_model",
-    
-    # Error message for Ref model management
-    # 'ref_file_empty' : "Reference model file is empty.",
-    # 'wrong_ref_name' : "Reference model name must contain minimum 3 characters and maximum 30 characters. English, Thai, number, a space ( ) and underscore (_) are allow.",
-    # 'wrong_unit_id' : "Wrong unit id. This ID dose not match any unit id on dpml_unit.",
-    # 'wrong_width' : "Wrong width. Width must be either int or float.",
-    # 'wrong_height' : "Wrong height. Height must be either int or float.",
-
-    # Error message for Log management
-    # 'wrong_date':"Wrong today date. Please re-enter today date.",
-    # 'wrong_group':"Wrong group. Please re-enter group.",
-    # 'file_not_exist':"File dose not exist.",
-    # 'wrong_start_date':"Wrong start date. Please re-enter start date.",
-    # 'wrong_end_date':"Wrong end date. Please re-enter end date.",
-    # 'wrong_start_datetime':"Wrong start datetime. Please re-enter start datetime.",
-    # 'wrong_end_datetime':"Wrong end datetime. Please re-enter end datetime.",
 }
